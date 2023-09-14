6eb821e4e9bd6ff0793e86615e0cb33b963236b0
Radmir <radmir_96@mail.ru>
2023-09-14 16:15:32 +0500

    calendar added

--- src/screens/CalendarScreen.jsx
+++ src/screens/CalendarScreen.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
-import {Text, View} from 'react-native'
+import {useState} from 'react'
+import {Calendar, LocaleConfig} from 'react-native-calendars'
+
+LocaleConfig.locales['ru'] = {
+  monthNames: [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ],
+  monthNamesShort: [
+    'Янв.',
+    'Февр.',
+    'Март',
+    'Апр.',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Авг.',
+    'Сент.',
+    'Окт.',
+    'Нояб.',
+    'Дек.',
+  ],
+  dayNames: [
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+    'Воскресенье',
+  ],
+  dayNamesShort: ['Пн.', 'Вт.', 'Ср.', 'Чт.', 'Пт.', 'Сб.', 'Вс.'],
+  today: 'Сегодня',
+}
+
+LocaleConfig.defaultLocale = 'ru'
 
 const CalendarScreen = () => {
+  const [selected, setSelected] = useState('')
+
   return (
-    <View>
-      <Text>CalendarScreen</Text>
-    </View>
+    <Calendar
+      onDayPress={day => {
+        setSelected(day.dateString)
+      }}
+      markedDates={{
+        [selected]: {
+          selected: true,
+          disableTouchEvent: true,
+          selectedDotColor: 'orange',
+        },
+      }}
+    />
   )
 }
 
