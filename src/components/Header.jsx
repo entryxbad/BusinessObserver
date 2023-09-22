07cd364a7570c04b187aeed9d4b2d73445a47933
Radmir <radmir_96@mail.ru>
2023-09-22 13:34:34 +0500

    add update button in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
-import {Text, View} from 'react-native'
+import {Text, TouchableOpacity, View} from 'react-native'
 import {UserCircleIcon} from 'react-native-heroicons/outline'
 
 const Header = () => {
   return (
-    <View className="flex-row bg-white w-full h-11 shadow-sm mb-3 items-center">
-      <View className="p-2 w-0">
+    <View className="flex-row bg-white w-full h-11 shadow-sm mb-3 items-center justify-between px-2">
+      <View className="w-0">
         <UserCircleIcon color={'red'} size={28} />
       </View>
-      <View className="mx-auto">
+      <View>
         <Text>Ufarobotics Mobile</Text>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Update</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
