17ad7b5a1b6ccb96f5455243bc594cdfac4a813f
exb <radmir_96@mail.ru>
2023-11-01 16:41:42 +0500

    change links

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,4 +1,4 @@
 const registerDeviceUrl =
-  'https://291f-92-50-180-214.ngrok-free.app/servLic/hs/licensing/registration'
+  'https://7601-92-50-180-214.ngrok-free.app/servLic/hs/licensing/registration'
 
 export {registerDeviceUrl}
